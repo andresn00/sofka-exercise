c562ff467b7900b27fdf1dab7b7ac14bbe9e513d
andresn00 <andreslozada00@hotmail.com>
2026-01-21 22:31:12 -0500

    Product form type

--- src/app/modules/products/types/products.types.ts
+++ src/app/modules/products/types/products.types.ts
@@ -1,3 +1,5 @@
+import { FormControl, FormGroup } from '@angular/forms';
+
 export type Product = {
   id: string;
   name: string;
@@ -8,3 +10,12 @@ export type Product = {
 };
 
 export type UpdateProduct = Omit<Product, 'id'>;
+
+export type FgProduct = FormGroup<{
+  id: FormControl<string>;
+  name: FormControl<string>;
+  description: FormControl<string>;
+  logo: FormControl<string>;
+  date_release: FormControl<string>;
+  date_revision: FormControl<string>;
+}>;

ec5d54357e506c844bd0caeb7a1356bc3159decc
andresn00 <andreslozada00@hotmail.com>
2026-01-21 21:22:01 -0500

    ProductsListPage basic setup

--- src/app/modules/products/pages/products-list/products-list.page.spec.ts
+++ src/app/modules/products/pages/products-list/products-list.page.spec.ts
@@ -1,3 +1,5 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsListPage } from './products-list.page';
@@ -9,6 +11,7 @@ describe('ProductListPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProductsListPage],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsListPage);
--- src/app/modules/products/pages/products-list/products-list.page.ts
+++ src/app/modules/products/pages/products-list/products-list.page.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { map } from 'rxjs';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [AsyncPipe, JsonPipe, RouterLink],
   templateUrl: './products-list.page.html',
   styleUrl: './products-list.page.scss',
 })
-export class ProductsListPage {}
+export class ProductsListPage {
+  private productsService = inject(ProductsService);
+
+  products$ = this.productsService.getProducts().pipe(map((res) => res.data));
+}

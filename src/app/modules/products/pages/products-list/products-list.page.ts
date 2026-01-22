7c61af673079240ba126f41990028008ba1eb5b4
andresn00 <andreslozada00@hotmail.com>
2026-01-21 23:12:36 -0500

    Filter products

--- src/app/modules/products/pages/products-list/products-list.page.ts
+++ src/app/modules/products/pages/products-list/products-list.page.ts
@@ -1,20 +1,45 @@
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { map } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, shareReplay, startWith, switchMap } from 'rxjs';
 import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-product-list',
-  imports: [AsyncPipe, JsonPipe, RouterLink],
+  imports: [AsyncPipe, JsonPipe, RouterLink, ReactiveFormsModule],
   templateUrl: './products-list.page.html',
   styleUrl: './products-list.page.scss',
 })
 export class ProductsListPage {
   private productsService = inject(ProductsService);
+  private fb = inject(FormBuilder);
+
+  fcSearch = this.fb.nonNullable.control('');
 
   // TODO handle products state
-  products$ = this.productsService.getProducts().pipe(map((res) => res.data));
+  products$ = this.productsService.getProducts().pipe(
+    map((res) => res.data),
+    shareReplay({ bufferSize: 1, refCount: true })
+  );
+
+  filteredProducts$ = this.fcSearch.valueChanges.pipe(
+    debounceTime(300),
+    distinctUntilChanged(),
+    startWith(this.fcSearch.value),
+    switchMap((search) =>
+      !search
+        ? this.products$
+        : this.products$.pipe(
+            map((products) =>
+              products.filter((product) => {
+                // TODO normalize texts
+                return product.name.includes(search);
+              })
+            )
+          )
+    )
+  );
 
   onDelete(id: string) {
     this.productsService.deleteProduct(id).subscribe(() => {

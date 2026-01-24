1a6b59b14f239c0b618821f6e24b3040ac8bfb87
andresn00 <andreslozada00@hotmail.com>
2026-01-24 15:44:25 -0500

    Use FormField in ProductForm

--- src/app/modules/products/components/product-form/product-form.component.ts
+++ src/app/modules/products/components/product-form/product-form.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppFormsModule } from '../../../forms/app-forms.module';
 import { FgProduct } from '../../types/products.types';
 
 @Component({
   selector: 'app-product-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, AppFormsModule],
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.scss',
 })

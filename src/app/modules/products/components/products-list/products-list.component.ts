27b06bb4b5771c08a627b962a7cb3c8a0fd434b2
andresn00 <andreslozada00@hotmail.com>
2026-01-25 18:48:32 -0500

    Extract ProductsListComponent

---
+++ src/app/modules/products/components/products-list/products-list.component.ts
@@ -0,0 +1,19 @@
+import { DatePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { PopoverComponent } from '../../../../components/popover/popover.component';
+import { Product } from '../../types/products.types';
+
+@Component({
+  selector: 'app-products-list',
+  imports: [RouterLink, DatePipe, PopoverComponent],
+  templateUrl: './products-list.component.html',
+  styleUrl: './products-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductsListComponent {
+  products = input.required<Product[]>();
+  dateFormat = 'dd/MM/yyyy';
+
+  delete = output<string>();
+}

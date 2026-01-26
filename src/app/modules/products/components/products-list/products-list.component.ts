cc5bfaec79687ede5f85d5d70546ec21007527cb
andresn00 <andreslozada00@hotmail.com>
2026-01-25 19:23:10 -0500

    Delete fixes

--- src/app/modules/products/components/products-list/products-list.component.ts
+++ src/app/modules/products/components/products-list/products-list.component.ts
@@ -2,11 +2,12 @@ import { DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PopoverComponent } from '../../../../components/popover/popover.component';
+import { ButtonDirective } from '../../../../directives/button/button.directive';
 import { Product } from '../../types/products.types';
 
 @Component({
   selector: 'app-products-list',
-  imports: [RouterLink, DatePipe, PopoverComponent],
+  imports: [RouterLink, DatePipe, PopoverComponent, ButtonDirective],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
